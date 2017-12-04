9cdf0edef2786ea7271acf970af1d0681f133b4b
Ana María <anamariasosam@gmail.com>
2017-12-04 17:56:02 -0500

    add new subcategories businesses endpoint

--- api/controllers/subcategoryController.js
+++ api/controllers/subcategoryController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Subcategory = mongoose.model('Subcategory')
 const Category = mongoose.model('Category')
+const Business = mongoose.model('Business')
 const utils = require('../handlers/utils')
 
 exports.findAll = function(req, res) {
@@ -21,5 +22,12 @@ exports.create = function(req, res) {
 
   subcategory.save(function(err, subcategory) {
     utils.show(res, err, subcategory)
+  })
+}
+
+exports.business = function(req, res) {
+  console.log(req.params.subcategoryId)
+  Business.find({ subcategory: req.params.subcategoryId }).exec(function(err, business) {
+    utils.show(res, err, business)
   })
 }
--- api/routes/subcategoryRoutes.js
+++ api/routes/subcategoryRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/subcategories', subcategory.findAll)
 router.post('/subcategories', subcategory.create)
+router.get('/subcategories/:subcategoryId/businesses', subcategory.business)
 
 module.exports = router
