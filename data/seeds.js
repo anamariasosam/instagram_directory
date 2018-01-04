fe729d104da822dfb25102e927f900a3857fba9f
Ana María <anamariasosam@gmail.com>
2018-01-03 21:29:43 -0500

    create a function to get a subcategory id by giving the name of that

--- data/seeds.js
+++ data/seeds.js
@@ -1,21 +1,33 @@
 const db = require('../db/db')
+const mongoose = require('mongoose')
 const assert = require('assert')
 const Subcategory = require('../api/models/subcategory')
 const Business = require('../api/models/business')
-const subcategories = require('./subcategories.json')
-const businesses = require('./businesses.json')
+const subcategoriesData = require('./subcategories.json')
+const businessesData = require('./businesses.json')
+
+// function createCollections(model, data, number) {
+//   model.collection.insertMany(data, function(err,r) {
+//     assert.equal(null, err);
+//     assert.equal(number, r.insertedCount);
+//     db.close();
+//     console.log(r.insertedCount + " collections added")
+//   })
+// }
 
 db.then(() => {
-  // Subcategory.collection.insertMany(subcategories, function(err,r) {
-  //   assert.equal(null, err);
-  //   assert.equal(17, r.insertedCount);
-  //   db.close();
-  //   console.log(r.insertedCount + " collections added")
-  // })
-  Business.collection.insertMany(businesses, function(err, r) {
-    assert.equal(null, err)
-    assert.equal(141, r.insertedCount)
-    db.close()
-    console.log(r.insertedCount + ' collections added')
+  // Subcategory.find().remove();
+  // // Business.find().remove();
+  // createCollections(Subcategory, subcategoriesData, 17)
+  // // createCollections(Business, businessesData, 141)
+
+  getSubcategoryId('Bebés & Niños', (err, id) => {
+    console.log(id)
   })
 })
+
+function getSubcategoryId(name, callback) {
+  Subcategory.findOne({ name }, '_id', (err, subcategory) => callback(null, subcategory._id))
+}
+
+// var id = new mongoose.Types.ObjectId(stringId);
