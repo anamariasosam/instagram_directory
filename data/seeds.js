bb94ae8b6f43375b017748bec7aa53cd5282524d
Ana María <anamariasosam@gmail.com>
2018-01-04 23:51:34 -0500

    add subcategories id dynamically

--- data/seeds.js
+++ data/seeds.js
@@ -1,33 +1,43 @@
 const db = require('../db/db')
-const mongoose = require('mongoose')
+const ObjectId = require('mongoose').Types.ObjectId
 const assert = require('assert')
 const Subcategory = require('../api/models/subcategory')
 const Business = require('../api/models/business')
 const subcategoriesData = require('./subcategories.json')
 const businessesData = require('./businesses.json')
 
-// function createCollections(model, data, number) {
-//   model.collection.insertMany(data, function(err,r) {
-//     assert.equal(null, err);
-//     assert.equal(number, r.insertedCount);
-//     db.close();
-//     console.log(r.insertedCount + " collections added")
-//   })
-// }
-
-db.then(() => {
-  // Subcategory.find().remove();
-  // // Business.find().remove();
-  // createCollections(Subcategory, subcategoriesData, 17)
-  // // createCollections(Business, businessesData, 141)
-
-  getSubcategoryId('Bebés & Niños', (err, id) => {
-    console.log(id)
+// Add the categoryId to all subcategories
+function addCategoryId(subcategories) {
+  subcategories.map(subcategory => {
+    subcategory.category = new ObjectId('5a26f20c465da00935ebc239')
   })
-})
-
-function getSubcategoryId(name, callback) {
-  Subcategory.findOne({ name }, '_id', (err, subcategory) => callback(null, subcategory._id))
 }
 
-// var id = new mongoose.Types.ObjectId(stringId);
+// Add the subcategoryId to all businesses
+function addsubcategoriesId(businesses) {
+  return businesses.map(async business => {
+    const { _id: id } = await Subcategory.findOne({ name: business.subcategory }).select('_id')
+    business.subcategory = id
+    return business
+  })
+}
+
+// create the collections for a specific model
+function createCollections(model, data, number) {
+  return model.collection.insertMany(data, function(err, r) {
+    db.close()
+    console.log(r.insertedCount + ' collections added')
+    process.exit(1)
+  })
+}
+
+db.then(async () => {
+  // Subcategory.find().remove();
+  // addCategoryId(subcategoriesData)
+  // createCollections(Subcategory, subcategoriesData, 17)
+
+  await Business.find().remove()
+  const data = await Promise.all(addsubcategoriesId(businessesData))
+  console.log(data)
+  await createCollections(Business, data, 141)
+})
